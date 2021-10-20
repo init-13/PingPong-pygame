a221f2f9b5393cf7a05ffa59d50f5a0222651f85
Nitin Das <nitinhash1308@gmail.com>
2021-10-21 01:45:41 +0530

    added music and background and pedal and ball

--- ballgame.py
+++ ballgame.py
@@ -24,6 +24,8 @@ total_score=0
 score_flag=0
 cpaddle_x=d_w-10
 #cpaddle_y=d_h-10
+pedalhit = pygame.mixer.Sound('pedalhit.wav')
+background = pygame.image.load('bkg.jpg')
 
 display=pygame.display.set_mode((d_w,d_h))
 
@@ -32,6 +34,7 @@ pygame.display.set_caption("Ping Pong!")
 def sign():
         for x in range(230):
                 display.fill((0,0,0))
+                display.blit(pygame.image.load('start.jpg'),(0,0))
                 sign_font=pygame.font.Font(None,25)
                 signature=sign_font.render("*** Created by: NITIN DAS ***",True,(255,255,255))
                 signature_rect=signature.get_rect(center=(int(d_w/2),int(d_h/2)))
@@ -39,8 +42,10 @@ def sign():
                 pygame.display.flip()
         return None
 def hit_back():
-        if x+radius>d_w - 10:
+        if x+radius>d_w - 14:
+                pedalhit.play()
                 return True
+
         return False
 
 def hit_sides():
@@ -57,27 +62,28 @@ def hit_paddle():
                 if (not score_flag):
                         total_score+=10
                         score_flag=1
+                pedalhit.play()
                 return True
         return False
 def game_over():
         end_game=True
-        font_title=pygame.font.Font(None,36)
+        font_title=pygame.font.Font(None,50)
         font_instructions=pygame.font.Font(None,24)
 
         announcement = font_title.render("GAME OVER",True,(255,255,255))
-        announcement_rect=announcement.get_rect(center=(int(d_w/2),int(d_h/2)))
+        announcement_rect=announcement.get_rect(center=(int(d_w/1.9),int(d_h/2)))
         display.blit(announcement,announcement_rect)
 
-        sinstructions= font_instructions.render("TOTAL SCORE : "+str(total_score),True,(255,255,255))
-        sinstructions_rect=sinstructions.get_rect(center=(int(d_w/2),int(d_h/1.77)))
+        sinstructions= font_instructions.render("SCORE : "+str(total_score),True,(255,255,255))
+        sinstructions_rect=sinstructions.get_rect(center=(int(d_w/1.7),int(d_h/1.77)))
         display.blit(sinstructions,sinstructions_rect)
 
         qinstructions= font_instructions.render("Press q to quit the game",True,(255,255,255))
-        qinstructions_rect=qinstructions.get_rect(center=(int(d_w/2),int(d_h/1.5)))
+        qinstructions_rect=qinstructions.get_rect(center=(int(d_w/3),50))
         display.blit(qinstructions,qinstructions_rect)
 
         rinstructions = font_instructions.render("Press r to play agin",True,(255,255,255))
-        rinstructions_rect=rinstructions.get_rect(center=(int(d_w/2),int(d_h/1.36)))
+        rinstructions_rect=rinstructions.get_rect(center=(int(d_w/1.3),50))
         display.blit(rinstructions,rinstructions_rect)
         pygame.display.flip()
         while(end_game):
@@ -93,11 +99,12 @@ def game_over():
                                         end_game=False
 
 display.fill((0,0,0))
+display.blit(pygame.image.load('start.jpg'),(0,0))
 welcome_screen=pygame.font.Font(None,30)
 welcome=welcome_screen.render("PING PONG",True,(255,255,255))
 welcome_rect=welcome.get_rect(center=(int(d_w/2),int(d_h/2)))
 display.blit(welcome,welcome_rect)
-startmsg=welcome_screen.render("Press y to start",True,(255,255,255))
+startmsg=welcome_screen.render("Press Y to start",True,(255,255,255))
 startmsg_rect=startmsg.get_rect(center=(int(d_w/2),int(d_h/1.43)))
 display.blit(startmsg,startmsg_rect)
 pygame.display.flip()
@@ -111,11 +118,14 @@ while(not start):
                                 start=True
                                 
 
+pedal=pygame.image.load('pedal.jpg')
+ball=pygame.image.load('ball.png')
 
 
 while True:
         clock.tick(speed)
         display.fill((0,0,0))
+        display.blit(background,(0,-30))
 
         pressed_key=pygame.key.get_pressed()
         if pressed_key[pygame.K_DOWN] and paddle_y+paddle_h+10 <=d_h:
@@ -130,15 +140,15 @@ while True:
         x+=dx
         y+=dy
         
-        pygame.draw.rect(display,(255,255,255),(paddle_x,paddle_y,paddle_w,paddle_h))
+        display.blit(pedal,(paddle_x,paddle_y,paddle_w,paddle_h))
 
-        pygame.draw.rect(display,(255,255,255),(cpaddle_x,y-30,paddle_w,paddle_h))
+        display.blit(pedal,(cpaddle_x,y-30,paddle_w,paddle_h))
 	
-        pygame.draw.circle(display,(255,255,255),(x,y),radius)
+        display.blit(ball,(x,y))
 
-        font_instructions=pygame.font.Font(None,30)
+        font_instructions=pygame.font.Font(None,60)
         sinstructions= font_instructions.render(str(total_score),True,(255,255,255))
-        sinstructions_rect=sinstructions.get_rect(center=(int(d_w/2),20))
+        sinstructions_rect=sinstructions.get_rect(center=(int(d_w/1.77),20))
         display.blit(sinstructions,sinstructions_rect)
 
         if not hit_paddle():
